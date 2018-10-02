1881c389218f273cbec8bcce75b267f914581eb5
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2018-10-02 17:40:14 +0200

    Operators

--- string class/string class/string.h
+++ string class/string class/string.h
@@ -1,48 +1,70 @@
-#pragma once
-
-#include <cstring>
-
 #ifndef _STRING_H_
 #define _STRING_H_
 
-typedef unsigned int uint;
-
-class string
+#include <cstring>
+class stringClass
 {
 private:
-	char*str = nullptr;
-	uint mem_allocation = 0;
+	char* str = nullptr;
+	unsigned int mem_allocation = 0;
 
 public:
-
 	//CONSTRUCTORS
-	string()
+	stringClass()
 	{
 		str = nullptr;
 		mem_allocation = 0;
 	}
 
-	string(const char *inputString)
+	stringClass(const char *inputString)
 	{
-		mem_allocation = strlen(inputString);
-		str = new char[mem_allocation + 1]; // +1 because all strings end in \0
-		strcpy(str, inputString);
+		if (inputString != NULL) {
+			mem_allocation = strlen(inputString);
+			str = new char[mem_allocation + 1]; // +1 because all strings end in \0
+			strcpy(str, inputString);
+		}
 	}
 
-	string(string& copyString)
+	stringClass(stringClass& inputString)
 	{
-		str = new char[copyString.mem_allocation];
-		strcpy(str, copyString.str);
+		mem_allocation = inputString.mem_allocation;
+		str = new char[mem_allocation];
+		strcpy(str, inputString.str);
 	}
 
 
 	//DESTRUCTOR THAT DELETES THE STRING
-	~string()
+	~stringClass()
+	{
+		if (str != NULL) delete[] str;
+	}
+public:
+
+	bool empty()const { return mem_allocation == 0; }
+
+	unsigned int size() { return mem_allocation; }
+
+	void clear()
+	{
+		str[0] = '\0';
+	}
+	/*stringClass operator= (stringClass &string)
 	{
 		delete[] str;
-		str = nullptr;
-	}
+		if (mem_allocation = string.size() != 0)
+		{
+			if(string.size <= mem_allocation)
+			{
 
+			}
+
+
+				str = new char[mem_allocation];
+			for (int i = 0; i < mem_allocation; i++)
+				str[i] = string[i];
+			return *this;
+		}
+	}*/
 };
 #endif // !_STRING_H_
 
